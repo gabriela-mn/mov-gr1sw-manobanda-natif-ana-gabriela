d9ad76161a7ff23252e1e540a9e84fe67b1c567b
gabriela-mn <cris_1997cm@hotmail.com>
2023-11-08 16:00:03 -0500

    comit inicial

---
+++ 02-Android/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "b2023-gr1sw-agmn"
+include(":app")
